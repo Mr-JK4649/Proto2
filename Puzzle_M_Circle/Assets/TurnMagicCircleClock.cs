f8f1c5bfb3fd4a35038d154e3c7291144007b8d4
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2022-02-16 11:57:57 +0900

    LRで魔法陣が回転するように

--- Puzzle_M_Circle/Assets/TurnMagicCircleClock.cs
+++ Puzzle_M_Circle/Assets/TurnMagicCircleClock.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnMagicCircle : MonoBehaviour
+public class TurnMagicCircleClock : MonoBehaviour
 {
 
     //距離を取得（定数）
@@ -10,7 +10,7 @@ public class TurnMagicCircle : MonoBehaviour
 
     //右回り用フラグ
     private bool canRightTurn;
-    
+
     //左回り用フラグ
     private bool canLeftTurn;
 
@@ -18,13 +18,16 @@ public class TurnMagicCircle : MonoBehaviour
     void Start()
     {
         Vector3 pos = GameObject.Find("Two").transform.localPosition;
-        OriDis = Vector2.Distance(Vector2.zero, new Vector2(pos.x, pos.z)); 
+        OriDis = Vector2.Distance(Vector2.zero, new Vector2(pos.x, pos.z));
         Debug.Log("OriDis : " + OriDis);
     }
 
     void Update()
     {
         TrunSelect();
+
+        TrunCircle();
+
     }
 
     private void TrunSelect()
@@ -46,13 +49,59 @@ public class TurnMagicCircle : MonoBehaviour
 
             child.localPosition = new Vector3(posX, 0, posZ);
         }
+        if (Input.GetButton("Fire4"))
+        {
+            RightTrun();
+        }
+        if (Input.GetButton("Fire5"))
+        {
+            LeftTrun();
+        }
+        if (canLeftTurn && !canRightTurn)
+        {
+
+        }
+    }
+
+    private void TrunCircle()
+    {
+        if (canRightTurn)
+        {
+            RightTrun();
+        }
+
+        if (canLeftTurn)
+        {
+
+        }
     }
 
     float GetAngle(Vector2 start, Vector2 target)
     {
         Vector2 dt = target - start;
         float rad = Mathf.Atan2(dt.y, dt.x);
+        float degree = rad * Mathf.Rad2Deg;
 
         return rad;
+    }
+
+    void RightTrun()
+    {
+
+    }
+
+    void LeftTrun()
+    {
+        foreach (Transform child in this.transform)
+        {
+            float ang = GetAngle(Vector2.zero, new Vector2(child.localPosition.x, child.localPosition.z));
+
+            float posX = Mathf.Cos(ang) * OriDis;      //X軸の設定
+            float posZ = Mathf.Sin(ang) * OriDis;      //Z軸の設定
+
+            ang -= 2 * Mathf.PI * Time.deltaTime;
+
+            child.localPosition = new Vector3(posX, 0, posZ);
+        }
     }
 }
